4554490dba6a1ee45534b208a7a37b6fe9f15047
tlock1 <thomaslock1995@gmail.com>
2024-10-10 02:21:03 +0200

    Add files via upload

---
+++ conversion.py
@@ -0,0 +1,20 @@
+
+# Step 1: State the variables: num1, num2, num3, string1
+# Step 2: Convert num1 to an integer
+# Step 3: Convert num2 to a float
+# Step 4: Convert num3 to a string
+# Step 5: Convert string1 to an integer
+# Step 6: Print each variable on separate lines
+
+num1 = 99.23
+num2 = 23
+num3 = 150
+string1 = "100"
+num1_int = int(num1)
+num2_float = float(num2)
+num3_str = str(num3)
+string1_int = int(string1)
+print(num1_int)
+print(num2_float)
+print(num3_str)
+print(string1_int)
